0824b4236c2d4b9706969ff7bda58829eabdbe1f
Zenwinks <Lulu180299>
2023-11-15 20:38:45 +0100

    cors everyone access

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(cors());
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", FRONT_APP_URL);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
